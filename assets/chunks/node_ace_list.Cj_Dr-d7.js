4e8a1a684242c5eedaa81dc377beb0b48a9a42a8
卢忠伟 <lucodestation@163.com>
2026-09-22 23:16:22 +0800

    配置站点将部署到的 base URL

---
+++ assets/chunks/node_ace_list.Cj_Dr-d7.js
@@ -0,0 +1,1 @@
+const s="/VitePress-AdonisJS-V7-Docs-Dist/assets/node_ace_list.DT4JevYz.png";export{s as _};
---
+++ assets/chunks/vscode_routes_list.BYndfdbO.js
@@ -0,0 +1,1 @@
+const s="/VitePress-AdonisJS-V7-Docs-Dist/assets/vscode_routes_list.fHp1w53a.png";export{s as _};
